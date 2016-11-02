48b4a5d3484df97670d6d2f9dc5a2ae1ae5fcfaa
YutoKumagai <b1013130@fun.ac.jp>
2016-11-02 11:56:22 +0900

    serviceを使ってログを出力した

--- app/src/main/java/com/example/yutokumagai/myservicetest/MainActivity.java
+++ app/src/main/java/com/example/yutokumagai/myservicetest/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.yutokumagai.myservicetest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button startButton;
+    Button stopButton;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+
+        startButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(getBaseContext(),MyService.class));
+            }
+        });
+
+        stopButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(getBaseContext(),MyService.class));
+            }
+        });
+    }
+
+    protected void findViews(){
+        startButton = (Button)findViewById(R.id.startButton);
+        stopButton = (Button)findViewById(R.id.stopButton);
     }
 }
